bc5356c8f3941b2c422c0d766059496818897841
Thomas Dulka <tjdulka@us.ibm.com>
2017-04-12 18:04:25 +0000

    New user List New files added - [hrdb,sourcesanspro-regular.ttf,ReadMe.txt,glyphicons-halflings-regular.ttf,Albertsons__2_.png,.wmproject.properties,theme.png,sourcesanspro-regular.woff2,glyphicons-halflings-regular.svg,sourcesanspro-regular.svg,style.less,sourcesanspro-regular.eot,hrdb.properties,hrdb.log,sourcesanspro-regular.woff,style.css,glyphicons-halflings-regular.woff,variables.less,hrdb.script,glyphicons-halflings-regular.eot,glyphicons-halflings-regular.woff2] Files modified - [leftnav.html,Main.variables.json,Main.html,Main.js,header.html,project-services.xml,pom.xml,deployment.properties,index.html,.wmproject.properties,wmProperties.js,development.properties] Removed files - [Roboto-Medium.otf,glyphicons-halflings-regular.woff2,.wmproject.properties,style.less,Roboto-Regular.ttf,Roboto-Medium.svg,style.css,glyphicons-halflings-regular.woff,glyphicons-halflings-regular.svg,Roboto-Regular-webfont.woff,Roboto-Regular-webfont.eot,glyphicons-halflings-regular.ttf,Roboto-Medium.eot,Roboto-Medium.ttf,Roboto-Medium.woff,Roboto-Medium.woff2,Roboto-Regular-webfont.svg,Roboto-Regular-webfont.ttf,glyphicons-halflings-regular.eot,variables.less,theme.png]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -7,7 +7,7 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,5 +1,5 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "material",
+  "activeTheme" : "cool-blue",
   "dateFormat" : "",
   "defaultLanguage" : "en",
   "displayName" : "TJD-HRPORTAL",
