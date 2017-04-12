c355249e407aa38e8e895f0e483ec36d4f96897e
root <root@6b8a8f8f3348>
2017-04-12 17:35:23 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TJD-HRPORTAL",
+  "homePage" : "Main",
+  "name" : "TJD_HRPORTAL",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
