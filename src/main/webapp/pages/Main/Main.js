443836119df21bdf1db8c8a396136f81f3e04acd
Thomas Dulka <tjdulka@us.ibm.com>
2017-04-14 12:45:55 +0000

    test output New files added - [svcOMSOrdersByProduct] Files modified - [Main.variables.json,Main.html,Main.js,project-services.xml,pom.xml,deployment.properties,hrdb.properties,development.properties]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,8 +1,8 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -12,6 +12,15 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
+    };
+
+    $scope.askWatsonWatsonresponse = function($isolateScope) {
+        var data = $isolateScope.watsonresponse;
+        var output_text = _.get(data, 'output.text');
+        console.log("success", output_text);
+        var entities = _.get(data, 'entities');
+        console.log("success", entities);
+
     };
 
 }]);
